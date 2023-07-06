bd91b39c46d92308477f7c3c51fe0bb4b836784f
narogishin <94650561+narogishin@users.noreply.github.com>
2023-07-06 18:27:10 +0100

    modified:   main.py

--- main.py
+++ main.py
@@ -6,13 +6,6 @@ import pandas as pd
 
 arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
 
-data = {
-    "time":[],
-    "CPU":[],
-    "memory":[],
-    "cpu_temp":[],
-        }
-
 while True :
     data = pd.read_csv('data.csv')
     new_data = {
@@ -21,10 +14,6 @@ while True :
     "memory":psutil.virtual_memory().percent,
     "cpu_temp":arduino.readline().decode().rstrip(),
         }
-    # data['time'].append(datetime.now())
-    # data['CPU'].append(psutil.cpu_percent(interval=1))
-    # data['memory'].append(psutil.virtual_memory().percent)
-    # data['cpu_temp'].append(arduino.readline().decode().rstrip())
     new_data = pd.DataFrame([new_data])
     df = pd.concat([data, new_data], ignore_index=True)
     df.to_csv("data.csv", index=False)
